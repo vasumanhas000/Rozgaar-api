dca8b5f532a294f3dbf47cebcea28f542a8a5434
vasumanhas000 <vasumanhas000@gmail.com>
2021-05-28 12:23:26 +0530

    Added routers

--- src/models/worker.js
+++ src/models/worker.js
@@ -9,22 +9,10 @@ const workerSchema = new mongoose.Schema(
     phone: {
       type: Number,
       required: true,
-      validate(value) {
-        var re = "^[6-9]d{9}$";
-        if (!re.test(value)) {
-          throw new Error("Phone Number is invalid");
-        }
-      },
     },
     pinCode: {
       type: Number,
       required: true,
-      validate(value) {
-        var re = "^[1-9][0-9]{5}$";
-        if (!re.test(value)) {
-          throw new Error("Phone Number is invalid");
-        }
-      },
     },
   },
   {
--- src/routers/worker.js
+++ src/routers/worker.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = new express.Router();
+const auth = require("../middleware/auth");
+const getUser = require("../functions/getUser");
 const Worker = require("../models/worker");
 
+// Get /workers?limit=10&skip=10
 router.get("/workers", auth, getUser, async (req, res) => {
   const pincode = req.query.pincode;
   const skip = req.query.skip;
@@ -12,6 +15,25 @@ router.get("/workers", auth, getUser, async (req, res) => {
       .limit(limit);
 
     res.status(202).send(workers);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+// bulk adds workers using ml model
+router.post("/workers/bulkAdd", auth, getUser, async (req, res) => {
+  const workers = req.body.workers;
+  try {
+    workers.forEach(async worker => {
+      var object = new Worker(worker);
+      try {
+        await object.save();
+      } catch (err) {
+        console.log(err);
+        return;
+      }
+    });
+    res.status(200).send(workers);
   } catch (err) {
     res.status(404).send(err);
   }
